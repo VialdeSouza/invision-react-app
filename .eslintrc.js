611090c1894fe0574a775dc975a890aa70a692f3
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-01 22:56:06 -0400

    chore: config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'linebreak-style': ['warn', 'windows'],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+  },
+};
