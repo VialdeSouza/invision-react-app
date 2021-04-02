113dc27ed786792a40649fa682a9f66975e949ef
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 14:49:27 -0400

    test: ensure render form to signup

---
+++ src/pages/signup.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Signup from './signup';
+
+describe('Signup Form', () => {
+  test('should render form to signup', () => {
+    render(<Signup />);
+    const nameInput = screen.getByRole('textbox', { name: /full name/i });
+    const emailInput = screen.getByRole('textbox', { name: /users name or Email/i });
+    const passwordInput = screen.getByRole('textbox', { name: /create password/i });
+    expect(nameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+  });
+});
