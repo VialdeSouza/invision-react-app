6a8f64a347a9e3c80f0cfd228c2367fd09619cc5
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 21:30:54 -0400

    refactor: use FieldName component

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import Field from '../components/field';
 import FieldEmail from '../components/fieldEmail';
+import FieldName from '../components/fieldName';
 import FieldPassword from '../components/fieldPassword';
 
 const Signup = () => (
   <div>
     <form>
 
-      <Field
-        label="Full Name"
-        id="name"
+      <FieldName
         onChange={() => {}}
         value=""
-        error=""
       />
 
       <FieldEmail
