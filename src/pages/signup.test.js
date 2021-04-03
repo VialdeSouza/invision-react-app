b80cb3c53232bbb267979e9b963466835d419595
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 21:56:27 -0400

    test: ensure navigate to SignIn when click Log in

--- src/pages/signup.test.js
+++ src/pages/signup.test.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
 import Signup from './signup';
 import { signUp } from '../core/signup-core';
 
 jest.mock('../core/signup-core');
 
+const renderWithProviders = async (component, history) => render(
+  <Router history={history || createMemoryHistory()}>
+    {component}
+  </Router>,
+);
 describe('Signup Form', () => {
   test('should render form to signup', () => {
-    render(<Signup />);
+    renderWithProviders(<Signup />);
     const nameInput = screen.getByRole('textbox', { name: /full name/i });
     const emailInput = screen.getByRole('textbox', { name: /users name or Email/i });
     const passwordInput = screen.getByRole('textbox', { name: /create password/i });
@@ -17,13 +24,13 @@ describe('Signup Form', () => {
   });
 
   test('should call  button Sign up', () => {
-    render(<Signup />);
+    renderWithProviders(<Signup />);
     const button = screen.getByRole('button', { name: 'Sign up' });
     expect(button).toBeInTheDocument();
   });
 
   test('should call signUp on submit form', () => {
-    render(<Signup />);
+    renderWithProviders(<Signup />);
     const nameInput = screen.getByRole('textbox', { name: /full name/i });
     const emailInput = screen.getByRole('textbox', { name: /users name or Email/i });
     const passwordInput = screen.getByRole('textbox', { name: /create password/i });
@@ -36,5 +43,21 @@ describe('Signup Form', () => {
     fireEvent.click(button);
 
     expect(signUp).toHaveBeenCalledWith({ name: 'any full name', email: 'any email', password: 'any password' });
+  });
+
+  test('should render link Log In', () => {
+    renderWithProviders(<Signup />);
+    const link = screen.getByRole('link', { name: /Log in/i });
+    expect(link).toBeInTheDocument();
+  });
+
+  test('should navigate to SignIn when click Log in', () => {
+    const history = createMemoryHistory();
+    const pushSpy = jest.spyOn(history, 'push');
+    renderWithProviders(<Signup />, history);
+    const link = screen.getByRole('link', { name: /Log in/i });
+    fireEvent.click(link);
+    expect(pushSpy).toHaveBeenCalledWith('/');
+    expect(history.location.pathname).toBe('/');
   });
 });
