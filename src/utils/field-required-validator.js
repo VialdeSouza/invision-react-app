dc7dd478346ea42ff6b8ce09213badf5431d1bf1
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 22:21:29 -0400

    feat: ensure fieldRequiredValidator validate string

--- src/utils/field-required-validator.js
+++ src/utils/field-required-validator.js
@@ -1,2 +1,8 @@
-const fieldRequiredValidator = () => ({ isValid: false, errorMessage: 'Este campo não pode ser vazio' });
+const fieldRequiredValidator = (value) => {
+  const clean = value.replace(/\s+/g, '');
+  if (clean.length) {
+    return ({ isValid: true, errorMessage: '' });
+  }
+  return ({ isValid: false, errorMessage: 'Este campo não pode ser vazio' });
+};
 export default fieldRequiredValidator;
