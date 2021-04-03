326f5c501f700e56fb12319bd4a28e4c9c9c49ef
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 22:53:39 -0400

    feat: ensure passwordValidator  returns error if receive void password

--- src/utils/password-validator.js
+++ src/utils/password-validator.js
@@ -1,2 +1,11 @@
-const passwordValidator = () => ({ isValid: false, errorMessage: 'any rule was broken' });
+import fieldRequiredValidator from './field-required-validator';
+
+const passwordValidator = (password) => {
+  const { isValid, errorMessage } = fieldRequiredValidator(password);
+  if (!isValid) {
+    return ({ isValid, errorMessage });
+  }
+
+  return ({ isValid: false, errorMessage: 'any rule was broken' });
+};
 export default passwordValidator;
