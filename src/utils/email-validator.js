98c7fe831c68fb32daf7cb48353dde3f03f2452f
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 22:35:02 -0400

    feat: ensure emailValidator returns error if receive invalid email

--- src/utils/email-validator.js
+++ src/utils/email-validator.js
@@ -1,9 +1,15 @@
+import validator from 'validator';
 import fieldRequiredValidator from './field-required-validator';
 
-const emailValidator = (value) => {
-  const { isValid, errorMessage } = fieldRequiredValidator(value);
+const emailValidator = (email) => {
+  const { isValid, errorMessage } = fieldRequiredValidator(email);
   if (!isValid) {
     return ({ isValid, errorMessage });
+  }
+  const isEmail = validator.isEmail(email);
+
+  if (!isEmail) {
+    return ({ isValid: false, errorMessage: 'O email está incorreto' });
   }
   return ({ isValid: false, errorMessage: 'any rule was broken' });
 };
