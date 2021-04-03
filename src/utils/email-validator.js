2814bb141ce03f238a31e80e309e2c57ca706cd2
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 22:26:43 -0400

    feat: ensure emailValidator returns error if receive void value

--- src/utils/email-validator.js
+++ src/utils/email-validator.js
@@ -1,2 +1,10 @@
-const emailValidator = () => ({ isValid: false, errorMessage: 'any rule was broken' });
+import fieldRequiredValidator from './field-required-validator';
+
+const emailValidator = (value) => {
+  const { isValid, errorMessage } = fieldRequiredValidator(value);
+  if (!isValid) {
+    return ({ isValid, errorMessage });
+  }
+  return ({ isValid: false, errorMessage: 'any rule was broken' });
+};
 export default emailValidator;
