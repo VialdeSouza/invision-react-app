d48c402b43dd9b4611366356ddc6cf1a1693997f
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-03 12:41:40 -0400

    feat: create global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import AuthRoutes from './routes/auth.routes';
+import { GlobalStyle } from './styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <AuthRoutes />
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/styles/global.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle` 
+  :root {
+  --main-color: #707070;
+  --contrast-main-color: #FFFF;
+  --error-color: #DC143C;
+  --green-color: #A9C5BA;
+  --small-font: 0.875rem;
+  --default-font: 1rem;
+  --medium-font: 1.25rem;
+  --large-font: 1.875rem;
+  --biggest-font: 2.188rem;
+  --main-font: 'Mulish', sans-serif;
+  --secondary-font: 'Lato', sans-serif;
+  }
+
+  * {    
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  body {
+    background: #FFFFFF;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: sans-serif;
+  }
+`;
