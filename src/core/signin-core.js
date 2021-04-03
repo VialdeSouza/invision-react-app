a31c59543684206908381824592b89896fc567c5
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 23:37:28 -0400

    feat: create fake signin and signup

--- src/core/signin-core.js
+++ src/core/signin-core.js
@@ -1,1 +1,15 @@
-export const signIn = () => 'done';
+import emailValidator from '../utils/email-validator';
+import passwordValidator from '../utils/password-validator';
+
+export const signIn = (values) => {
+  const { email, password } = values;
+  const { isValid: emailIsValid } = emailValidator(email);
+  const { isValid: passwordIsValid } = passwordValidator(password);
+  const isValid = emailIsValid && passwordIsValid;
+  if (!isValid) {
+    alert('fails');
+    return 'fails';
+  }
+  alert('done');
+  return 'done';
+};
--- src/core/signup-core.js
+++ src/core/signup-core.js
@@ -1,1 +1,17 @@
-export const signUp = () => 'done';
+import emailValidator from '../utils/email-validator';
+import fieldRequiredValidator from '../utils/field-required-validator';
+import passwordValidator from '../utils/password-validator';
+
+export const signUp = (values) => {
+  const { name, email, password } = values;
+  const { isValid: nameIsValid } = fieldRequiredValidator(name);
+  const { isValid: emailIsValid } = emailValidator(email);
+  const { isValid: passwordIsValid } = passwordValidator(password);
+  const isValid = nameIsValid && emailIsValid && passwordIsValid;
+  if (!isValid) {
+    alert('fails');
+    return 'fails';
+  }
+  alert('done');
+  return 'done';
+};
