0e8cf029ecbebe1d103c9ca44db0e901d1516b5a
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 23:13:15 -0400

    fix: remove incorrect Route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import AuthRoutes from './routes/auth.routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <> </>
+    <AuthRoutes />
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Login } from '../pages/login';
 import Signup from '../pages/signup';
 
 const AuthRoutes = () => (
   <Router>
-    <Router path="/" exact component={Login} />
-    <Router path="/signup" component={Signup} />
+    <Route path="/" exact component={Login} />
+    <Route path="/signup" component={Signup} />
   </Router>
 );
 
