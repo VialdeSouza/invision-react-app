918f978b0f06f9ad9c4518b596283a50f0a22ae3
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 21:48:30 -0400

    test: ensure call signUp on submit Signup form

--- src/pages/signup.test.js
+++ src/pages/signup.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import Signup from './signup';
-import passwordValidator from '../utils/password-validator';
+import { signUp } from '../core/signup-core';
 
-jest.mock('../utils/password-validator');
+jest.mock('../core/signup-core');
+
 describe('Signup Form', () => {
   test('should render form to signup', () => {
     render(<Signup />);
@@ -19,5 +20,21 @@ describe('Signup Form', () => {
     render(<Signup />);
     const button = screen.getByRole('button', { name: 'Sign up' });
     expect(button).toBeInTheDocument();
+  });
+
+  test('should call signUp on submit form', () => {
+    render(<Signup />);
+    const nameInput = screen.getByRole('textbox', { name: /full name/i });
+    const emailInput = screen.getByRole('textbox', { name: /users name or Email/i });
+    const passwordInput = screen.getByRole('textbox', { name: /create password/i });
+
+    fireEvent.change(nameInput, { target: { value: 'any full name' } });
+    fireEvent.change(emailInput, { target: { value: 'any email' } });
+    fireEvent.change(passwordInput, { target: { value: 'any password' } });
+
+    const button = screen.getByRole('button', { name: 'Sign up' });
+    fireEvent.click(button);
+
+    expect(signUp).toHaveBeenCalledWith({ name: 'any full name', email: 'any email', password: 'any password' });
   });
 });
