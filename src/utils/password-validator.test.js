45f7a249dfe5c6c571246419c67820f2d62c06df
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 22:58:51 -0400

    test: ensure passwordValidator returns true if receive valid password

--- src/utils/password-validator.test.js
+++ src/utils/password-validator.test.js
@@ -6,4 +6,16 @@ describe('passwordValidator', () => {
     expect(isValid).not.toBeTruthy();
     expect(errorMessage).toEqual('Este campo não pode ser vazio');
   });
+
+  test('should return error if receive invalid password', () => {
+    const { isValid, errorMessage } = passwordValidator('1234');
+    expect(isValid).not.toBeTruthy();
+    expect(errorMessage).toEqual('A senha não pode ter menos de 6 caracteres');
+  });
+
+  test('should return true if receive valid password', () => {
+    const { isValid, errorMessage } = passwordValidator('123456');
+    expect(isValid).toBeTruthy();
+    expect(errorMessage).toEqual('');
+  });
 });
