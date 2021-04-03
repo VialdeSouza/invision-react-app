89ad0aa481f431f370de2a47197f5f018a77a190
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-02 22:34:53 -0400

    test: ensure emailValidator returns error if receive invalid email

--- src/utils/email-validator.test.js
+++ src/utils/email-validator.test.js
@@ -1,9 +1,23 @@
+import validator from 'validator';
 import emailValidator from './email-validator';
+
+jest.mock('validator', () => ({
+  isEmail() {
+    return true;
+  },
+}));
 
 describe('emailValidator', () => {
   test('should return error if receive void value', () => {
     const { isValid, errorMessage } = emailValidator('   ');
     expect(isValid).not.toBeTruthy();
     expect(errorMessage).toEqual('Este campo não pode ser vazio');
+  });
+
+  test('should return error if receive invalid email', () => {
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
+    const { isValid, errorMessage } = emailValidator('invalid email');
+    expect(isValid).not.toBeTruthy();
+    expect(errorMessage).toEqual('O email está incorreto');
   });
 });
