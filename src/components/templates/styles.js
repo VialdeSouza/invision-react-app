635876e1fa17a748d0f73c6077569178590b2196
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-04 18:58:46 -0400

    feat: define height to Page

--- src/components/templates/styles.js
+++ src/components/templates/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Page = styled.div`
    display: flex;
    flex-direction: row;
+   height: 100vh;
    @media(max-width: 780px){
         flex-direction: column
    }
