f53fa3f5993751c066600556fd0ac54f08291b33
Tamyres Vial de Souza <vialdesouza@gmail.com>
2021-04-04 19:59:58 -0400

    refator: recycle code for render inputs Login

--- src/pages/login.test.js
+++ src/pages/login.test.js
@@ -15,42 +15,56 @@ const renderWithProviders = async (component, history) => render(
   </Router>,
 );
 
+const getInputs = () => {
+  const emailInput = screen.getByRole('textbox', { name: /users name or Email/i });
+  const passwordInput = screen.getByLabelText(/password/i);
+  return { emailInput, passwordInput };
+};
+const getButtons = () => {
+  const buttonSignIn = screen.getByRole('button', { name: 'Sign in' });
+  const socialButton = screen.getByRole('button', { name: /Sign in with Google/i });
+  return { buttonSignIn, socialButton };
+};
+const getLinks = () => {
+  const link = getLinks();
+  return { link };
+};
+
 describe('Login form', () => {
   test('should render form to login', () => {
     renderWithProviders(<Login />);
-    const emailInput = screen.getByRole('textbox', { name: /users name or Email/i });
-    const passwordInput = screen.getByLabelText(/password/i);
+    const { emailInput, passwordInput } = getInputs();
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
   });
 
   test('should render button Sign in', () => {
     renderWithProviders(<Login />);
-    const button = screen.getByRole('button', { name: 'Sign in' });
-    expect(button).toBeInTheDocument();
+    const { buttonSignIn } = getButtons();
+    expect(buttonSignIn).toBeInTheDocument();
   });
 
   test('should render button Sign in with Google', () => {
     renderWithProviders(<Login />);
-    const button = screen.getByRole('button', { name: /Sign in with Google/i });
-    expect(button).toBeInTheDocument();
+    const { socialButton } = getButtons();
+    expect(socialButton).toBeInTheDocument();
   });
 
   test('should call signIn on submit form', () => {
     renderWithProviders(<Login />);
-    const button = screen.getByRole('button', { name: 'Sign in' });
-    const emailInput = screen.getByRole('textbox', { name: /email/i });
-    const passwordInput = screen.getByLabelText(/password/i);
+    const { buttonSignIn } = getButtons();
+    const { emailInput, passwordInput } = getInputs();
+
     fireEvent.change(emailInput, { target: { value: 'any email' } });
     fireEvent.change(passwordInput, { target: { value: 'any password' } });
-    fireEvent.click(button);
+    fireEvent.click(buttonSignIn);
 
     expect(signIn).toHaveBeenCalledWith({ email: 'any email', password: 'any password' });
   });
 
   test('should render link Create Account', () => {
     renderWithProviders(<Login />);
-    const link = screen.getByRole('link', { name: /create account/i });
+    const { link } = getLinks();
     expect(link).toBeInTheDocument();
   });
 
@@ -58,7 +72,7 @@ describe('Login form', () => {
     const history = createMemoryHistory();
     const pushSpy = jest.spyOn(history, 'push');
     renderWithProviders(<Login />, history);
-    const link = screen.getByRole('link', { name: /create account/i });
+    const { link } = getLinks();
     fireEvent.click(link);
     expect(pushSpy).toHaveBeenCalledWith('/signup');
     expect(history.location.pathname).toBe('/signup');
